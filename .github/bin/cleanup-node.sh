7b4d78bee9aa1f51d8cc8a2756e58706bf9649a4
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2021-06-03 22:23:39 +0530

    stress ci

--- .github/bin/cleanup-node.sh
+++ .github/bin/cleanup-node.sh
@@ -6,8 +6,11 @@ echo "Space before cleanup"
 df -h
 
 echo "Removing redundant directories"
+sudo du -hs /opt/hostedtoolcache/go
 sudo rm -rf /opt/hostedtoolcache/go
+sudo du -hs /usr/local/lib/android
 sudo rm -rf /usr/local/lib/android
+sudo du -hs /usr/share/dotnet
 sudo rm -rf /usr/share/dotnet
 
 echo "Space after cleanup"
