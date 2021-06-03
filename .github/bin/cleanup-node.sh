1892ba84b02761676e89afb6965d8db239b9ac5d
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2021-06-04 02:51:25 +0530

    partially limit disk space (should become flaky)

--- .github/bin/cleanup-node.sh
+++ .github/bin/cleanup-node.sh
@@ -9,7 +9,7 @@ echo "Removing redundant directories"
 sudo du -hs /opt/hostedtoolcache/go
 #sudo rm -rf /opt/hostedtoolcache/go
 sudo du -hs /usr/local/lib/android
-#sudo rm -rf /usr/local/lib/android
+sudo rm -rf /usr/local/lib/android
 sudo du -hs /usr/share/dotnet
 #sudo rm -rf /usr/share/dotnet
 
