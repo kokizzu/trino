82f67b6f0c33a218cba4f9222eb87362d0876145
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2021-06-04 11:08:40 +0530

    limit disk space to 25GB

--- .github/bin/cleanup-node.sh
+++ .github/bin/cleanup-node.sh
@@ -13,5 +13,8 @@ sudo rm -rf /usr/local/lib/android
 sudo du -hs /usr/share/dotnet
 #sudo rm -rf /usr/share/dotnet
 
+# After removing above we have 28GB space free - let's bring that down to 25GB as reported in the original flaky test issue
+dd if=/dev/zero of=space_filler.img bs=1M count=3000
+
 echo "Space after cleanup"
 df -h
